72801a18147d07749128347691334cd14fe6b915
Sugata Acharjya <sugata.a@gmail.com>
2023-07-20 23:16:46 -0700

    getting started with swiftui complete

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Sugata Acharjya on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
